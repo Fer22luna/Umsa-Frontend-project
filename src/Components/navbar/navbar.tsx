363b08067a462eafc13c8379fbc3b1e833a98f5b
Fernando Gabriel Luna <fer22luna@gmail.com>
2024-07-06 20:17:35 -0300

    agregando botones para los forms

--- src/Components/navbar/navbar.tsx
+++ src/Components/navbar/navbar.tsx
@@ -14,7 +14,6 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from 'react-router-dom';
 
-//const pages = ['Products', 'Pricing', 'Blog'];
 const pages = [
   { name: 'Inicio', path: '/' },
   { name: 'Pacientes', path: '/pacientes' },
