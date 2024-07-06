5fbec04ca16dcb523047d679914316d1cdef15a2
Fernando Gabriel Luna <fer22luna@gmail.com>
2024-07-06 17:52:42 -0300

    agregando turnos

---
+++ src/models/types/TypeTurno.ts
@@ -0,0 +1,13 @@
+
+export interface TypeTurno {
+    
+    id?: string,
+    fechaHoraTurno: String,
+    doctor: string | undefined,
+    domicilioConsulta: string,
+    estadoTurno: boolean,
+    obraSocial: string,
+    paciente: string | undefined,
+
+}
+
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { Navigate, Routes, Route } from "react-router-dom"
 import Home from "../pages/home";
 import PacienteList from "../pages/PacienteList";
 import TurnoList from "../pages/TurnoList";
+import TurnoForm from "../pages/TurnoForm";
 import DoctoresList from "../pages/DoctorList";
 import EspecialidadList from "../pages/especialidades";
 import PacienteForm from "../pages/PacienteForm";
@@ -15,6 +16,7 @@ const Router = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/pacientes" element={<PacienteList />} />
                     <Route path="/pacienteform" element={<PacienteForm />} />
+                    <Route path="/turnoform" element={<TurnoForm />} />
                     <Route path="/doctores" element={<DoctoresList />} />
                     <Route path="/turnos" element={<TurnoList />} />
                     <Route path="/especialidades" element={<EspecialidadList />} />
